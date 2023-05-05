b44d937f31c8fec0240b9098a995f60338dc497b
GIOVANA KOSLOWSKI <giovana_koslowski@estudante.sesisenai.org.br>
2023-05-05 16:21:41 -0300

    versão final

--- loja.js
+++ loja.js
@@ -2,6 +2,7 @@ let salva = document.getElementById('salva')
 let calcula = document.getElementById('calcula')
 let res = document.getElementById('res')
 let produto_array = [], qtde_array = []
+let blusa = 35.50, calca = 55.50, bermuda = 45.50
 
 salva.addEventListener('click', ()=>{
     let produto = document.getElementById('produto').value
@@ -9,4 +10,19 @@ salva.addEventListener('click', ()=>{
 
     produto_array.push(produto)
     qtde_array.push(qtde)
+    console.log(produto_array)
+    console.log(qtde_array)
+})
+calcula.addEventListener('click', ()=>{
+    total = 0.0
+    for(i=0; i<produto_array.length; i++){
+        if(produto_array[i] = 'blusa'){
+            total = total + (blusa*qtde_array[i])
+        }else if(produto_array[i] = 'calca'){
+            total = total + (calca*qtde_array[i])
+        }else if(produto_array[i] = 'bermuda'){
+            total = total + (bermuda*qtde_array[i])
+        }
+    }
+    res.innerHTML = 'O valor total da sua compra é de R$ ' + total.toFixed(2)
 })
